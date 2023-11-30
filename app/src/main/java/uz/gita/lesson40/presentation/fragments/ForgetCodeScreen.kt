09a018180ac71fc9d04829733d006bba4150b407
RNurali <rasuljonovnurali04@gmail.com>
2023-11-30 17:00:55 +0500

    Rasuljonov5

--- app/src/main/java/uz/gita/lesson40/presentation/fragments/ForgetCodeScreen.kt
+++ app/src/main/java/uz/gita/lesson40/presentation/fragments/ForgetCodeScreen.kt
@@ -5,7 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import uz.gita.lesson40.R
 
-class ForgetCodeScreen : Fragment(R.layout.fragment_forget_code_screen) {
+class ForgetCodeScreen : Fragment(R.layout.update_number_screen) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
